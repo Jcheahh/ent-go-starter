938bf8addf5c367ca003ed4e52f3dee17a1cc176
jack <jackcheah2@gmail.com>
2023-05-19 23:37:04 +0800

    Migrate when push

--- ent/migrate/migrations/20230516142340_all_schema.sql
+++
@@ -1,6 +0,0 @@
--- Modify "notifications" table
-ALTER TABLE "notifications" ALTER COLUMN "date_created" SET DEFAULT '2023-05-16T22:23:39+08:00', ALTER COLUMN "date_updated" SET DEFAULT '2023-05-16T22:23:39+08:00';
--- Modify "shipping_addresses" table
-ALTER TABLE "shipping_addresses" ALTER COLUMN "date_created" SET DEFAULT '2023-05-16T22:23:39+08:00', ALTER COLUMN "date_updated" SET DEFAULT '2023-05-16T22:23:39+08:00';
--- Modify "users" table
-ALTER TABLE "users" ALTER COLUMN "date_created" SET DEFAULT '2023-05-16T22:23:39+08:00', ALTER COLUMN "date_updated" SET DEFAULT '2023-05-16T22:23:39+08:00';
--- ent/migrate/migrations/20230517143218_all_schema.sql
+++
@@ -1,6 +0,0 @@
--- Modify "notifications" table
-ALTER TABLE "notifications" ALTER COLUMN "date_created" SET DEFAULT '2023-05-17T22:32:17+08:00', ALTER COLUMN "date_updated" SET DEFAULT '2023-05-17T22:32:17+08:00';
--- Modify "shipping_addresses" table
-ALTER TABLE "shipping_addresses" ALTER COLUMN "date_created" SET DEFAULT '2023-05-17T22:32:17+08:00', ALTER COLUMN "date_updated" SET DEFAULT '2023-05-17T22:32:17+08:00';
--- Modify "users" table
-ALTER TABLE "users" ALTER COLUMN "date_created" SET DEFAULT '2023-05-17T22:32:17+08:00', ALTER COLUMN "date_updated" SET DEFAULT '2023-05-17T22:32:17+08:00';
